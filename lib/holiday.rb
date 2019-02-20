69ead74ae73e691d9dc4073d3c44f3dacb9c84c4
Lige <lige.williamson@gmail.com>
2019-02-20 13:33:25 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -80,5 +80,6 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   holiday_hash.each do |season, holiday|
-
+    holiday.collect do |holiday, supplies|
+      holiday if supplies.include?("BBQ")
 end
